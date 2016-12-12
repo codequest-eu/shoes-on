6617c36eed43b9afcdb4695b97b1835365048118
Pi <rpi@codequest.com>
2016-12-12 17:11:44 +0000

    simple button example

--- test.rb
+++ test.rb
@@ -1,1 +1,9 @@
-Shoes.app { button "Push me now!" }
+Shoes.app do
+  background '#F3F'..'#F90'
+  self.fullscreen = true
+  @push = button("Click Me ;__;", align: 'center', left: 20, top: 20)
+  @note = title('', top: 60, align: 'center', stroke: white)
+  @push.click do
+    @note.replace(@note.text.empty? ? "Hello!" : nil)
+  end
+end
