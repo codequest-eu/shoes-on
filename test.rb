0734b82b047d69c5e30d524fafa4b62318b7f84c
Pi <rpi@codequest.com>
2016-12-13 13:52:47 +0000

    experiment with flow and stack

--- test.rb
+++ test.rb
@@ -1,23 +1,27 @@
-# Shoes.app do
-#   @push = button("Click Me ;__;", align: 'center', left: 20, top: 20)
-#   @note = title("\xF0\x9F\x98\xBF", font: 'Bitstream Vera', top: 60, align: 'center', stroke: white)
-#   @push.click do
-#     @note.replace(@note.text.empty? ? "Hello" : nil)
-#   end
-# end
-
+# encoding: utf-8
 
 Shoes.app(title:"Shoes On!", width:(480), height:(320)) do
   background '#F3F'..'#F90'
   self.fullscreen = true
   @x = 0
   @y = 0
-  @map = Array.new(4) { Array.new(6) }
+
+  @map = [
+    [{id: 'btn1', text: '�~_~P�'}, {id: 'btn2', text: '�~_~X~B'}],
+    [{id: 'btn3', text: '�~_~X~N'}, {id: 'btn4', text: '�~_~Y~H'}],
+  ]
 
   @map.each_with_index do |row, r|
-    row.each_with_index do |item, c|
-      fill "%06x" % (rand * 0xffffff)
-      rect(top:(r * 80), left:(c * 80), width:80, height:80)
+     flow(height: 80) do
+      row.each_with_index do |item, c|
+        stack(width: 80) do
+          fill "%06x" % (rand * 0xffffff)
+          rect(width:80, height:80).click do
+            puts item[:id] # send event
+          end
+          para(item[:id], width: 80, height: 80)
+        end
+      end
     end
   end
 end
