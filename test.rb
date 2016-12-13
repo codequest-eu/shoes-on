b1ff07619356fda9ddb8458798b2f99a69ffc55f
Pi <rpi@codequest.com>
2016-12-13 10:19:34 +0000

    try to show an emoji

--- test.rb
+++ test.rb
@@ -2,8 +2,8 @@ Shoes.app do
   background '#F3F'..'#F90'
   self.fullscreen = true
   @push = button("Click Me ;__;", align: 'center', left: 20, top: 20)
-  @note = title('', top: 60, align: 'center', stroke: white)
+  @note = title("\xF0\x9F\x98\xBF", font: 'Bitstream Vera', top: 60, align: 'center', stroke: white)
   @push.click do
-    @note.replace(@note.text.empty? ? "Hello!" : nil)
+    @note.replace(@note.text.empty? ? "Hello" : nil)
   end
 end
