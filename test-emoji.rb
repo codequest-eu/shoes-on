9567fb7acf6eb4911a71b72a48c6409b9dabb984
Pi <rpi@codequest.com>
2016-12-13 11:49:27 +0000

    add files

---
+++ test-emoji.rb
@@ -0,0 +1,19 @@
+# encoding: utf-8
+
+Shoes.setup do
+  gem 'emoji'
+end
+
+require 'emoji'
+Emoji.asset_host = nil
+
+def emoji_image(str)
+  image "emoji/assets/images#{Emoji.image_url_for_unicode_moji(str)}"
+end
+
+Shoes.app do
+  self.fullscreen = true # this breaks shoes on mac
+  emoji_image('🍕')
+  emoji_image('😂')
+  emoji_image('🐳')
+end
