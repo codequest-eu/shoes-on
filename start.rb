e7ac2804756c9da406ef59954951cb156ccd1618
Pi <rpi@codequest.com>
2016-12-14 13:38:15 +0000

    add mock layout as json, add mqtt to start program

--- start.rb
+++ start.rb
@@ -2,35 +2,64 @@
 
 Shoes.setup do
   gem 'gemoji'
-  gem 'ruby-mqtt'
+  gem 'mqtt'
 end
 
 require 'emoji'
+require 'mqtt'
 
 def emoji_image(str)
   image "emojis/#{Emoji.find_by_unicode(str).image_filename}"
-rescue StandardError
+rescue StandardError => e
+  puts e.inspect
+  # emoji might be unsupported, print a sad face instead
   title ';__;'
 end
 
-Shoes.app(title:"Shoes On!", width:(480), height:(320)) do
-  background '#F3F'..'#F90'
-  #self.fullscreen = true # this breaks shoes on mac
+Shoes.app(title: "mojify", width: 480, height: 320) do 
+  # TODO(anyone): security! xD ;__;
+  @mqtt = MQTT::Client.connect("mqtts://difrxdkm:EIAdmFlDJlos@m21.cloudmqtt.com:28560")
+  
+  def set_bg_awesomeness
+    background '#F3F'..'#F90' # can't touch this! 
+  end
 
-  @map = [
-    [{id: 'btn1', text: '🙈'}, {id: 'btn2', text: '😂'}],
-    [{id: 'btn3', text: '🐗'}, {id: 'btn4', text: '💩'}],
-    [{id: 'btn1', text: '🙌'}, {id: 'btn4', text: '🐴'}],
-    [{id: 'btn1', text: '😚'}, {id: 'btn4', text:'🙆'}],
-  ]
-
-  @map.each_with_index do |row, r|
-     flow(height: 80) do
-      row.each_with_index do |item, c|
-        stack(width: 80) do
-          emoji_image(item[:text]).click { puts item[:id] }
+  def render(data_layout)
+    clear do
+      set_bg_awesomeness
+      data_layout.each_with_index do |row, r|
+        flow(height: 80) do
+          row.each_with_index do |item, c|
+            stack(width: 80) do
+              emoji_image(item['text']).click do |target|
+                 frames = 0
+                 icon_anim = animate(24) do |i|
+                   target.displace((Math.cos(i) * 4).to_i, (Math.sin(i) * 4).to_i)
+                   icon_anim.stop if frames > 32
+                   frames += 1
+                 end
+                 @mqtt.publish('events', item['text'])
+              end
+            end
+          end
         end
       end
     end
   end
+  
+  Thread.new do 
+    # TODO(anyone): security! xD ;__;
+    puts "subscribing..."
+    @mqtt.get('layouts') do |topic, message|
+      # TODO(anyone): validate message request and fail nicely
+      render(JSON.parse(message)) 
+    end 
+  end
+
+  # self.fullscreen = true # this breaks shoes on a mac
+
+  # TODO(anyone): prepare initial splash screea
+  set_bg_awesomeness
+  para("Hello!!!", top: 100, left: 100)
+  # end splash screen
 end
